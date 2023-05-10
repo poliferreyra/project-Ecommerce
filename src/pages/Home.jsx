b84d845e669aabf83b146f2a938a8fc87fd2bed1
poliferreyra <paoferreyra.fl@gmail.com>
2023-05-09 21:05:32 -0300

    Add imgHome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { Box, Flex, Image } from '@chakra-ui/react'
+import imgHome from '../assets/imgHome.png'
+
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <Flex justifyContent="center" minW="100vw" bg="#EC8F3C" mt={10}>
+      <Box maxW="60%" maxH="50%" m={3}>
+        <Image
+          objectFit="cover"
+          borderRadius="10px"
+          src={imgHome}
+          alt="imgHome"
+        />
+      </Box>
+    </Flex>
+  )
 }
