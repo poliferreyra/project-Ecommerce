42e0a02c1f4b73467c6b7c8488dc2f941d178002
poliferreyra <paoferreyra.fl@gmail.com>
2023-05-30 20:08:54 -0300

    Add spinner in favorites

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,27 +12,33 @@ import {
 } from '@chakra-ui/react'
 import { NavLink } from 'react-router-dom'
 // import womanhome from '../assets/womanhome.png'
+/* eslint-disable react/no-unescaped-entities */
 import { useGetProducts } from '../hook/useGetProducts'
 import { useContext } from 'react'
 import { CartContext } from '../context/CartContext'
+import { Spinner } from '../ui/spinner'
 
 export const Home = () => {
-  const { renderProducts } = useGetProducts()
+  const { renderProducts, loading, error } = useGetProducts()
   const { addProductToCart } = useContext(CartContext)
 
   return (
-    <Stack>
-      <HStack>
-        <Box w="100%" h="330px" position="relative">
-          <Box w="75%">
-            <Heading size="sm" p={10} color="black">
-              Welcome to our online perfume and cosmetics store! <br />
-              Discover a wide variety of high-quality products to enhance your
-              natural beauty and highlight your personal style.
-            </Heading>
-          </Box>
+    <>
+      {error && <Text>There's an error</Text>}
+      {!renderProducts.length && !loading && <Text>There're not products</Text>}
 
-          {/* <Image
+      <Stack>
+        <HStack>
+          <Box w="100%" h="330px" position="relative">
+            <Box w="75%">
+              <Heading size="sm" p={10} color="black">
+                Welcome to our online perfume and cosmetics store! <br />
+                Discover a wide variety of high-quality products to enhance your
+                natural beauty and highlight your personal style.
+              </Heading>
+            </Box>
+
+            {/* <Image
             w={{ base: '48%', md: '38%', lg: '30%', xl: '25%' }}
             src={womanhome}
             alt="Home Imgage"
@@ -41,69 +47,75 @@ export const Home = () => {
             right="-10"
           /> */}
 
-          <Link
-            p={2}
-            ml="30px"
-            color="white"
-            fontWeight="bold"
-            borderRadius="10px"
-            bg="#F29101"
-            borderColor="#282445"
-            as={NavLink}
-            to="/aboutUs"
-            _hover={{
-              fontWeight: 'semibold',
-              color: '#282445',
-            }}
-          >
-            About us
-          </Link>
-        </Box>
-      </HStack>
-      <HStack>
-        <Heading size="md">Favorite Products</Heading>
-      </HStack>
-      <SimpleGrid columns={{ base: 1, md: 3, lg: 4 }} p={2}>
-        {renderProducts.map((product) => (
-          <Box key={product.id} w="90%" h="80%" align="center">
-            <Box w="100%" p={2} m={1}>
-              <Image
-                src={product.img}
-                alt={product.prodName}
-                borderRadius="lg"
-                maxW="70%"
-                maxH="40%"
-              />
-              <Stack mt="6">
-                <Heading size="md">{product.prodName}</Heading>
-                <Text>{product.description}</Text>
-                <Text color="blue.600" fontSize="1xl">
-                  {`$ ${product.price}`}
-                </Text>
-              </Stack>
-              <ButtonGroup m={1}>
-                <Button
-                  as={NavLink}
-                  size={{ base: 'xs', md: 'sm' }}
-                  variant="solid"
-                  colorScheme="blue"
-                  to={`/products/${product.id}`}
-                >
-                  See detail
-                </Button>
-                <Button
-                  size={{ base: 'xs', md: 'sm' }}
-                  variant="solid"
-                  bg="tomato"
-                  onClick={() => addProductToCart(product, 1)}
-                >
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </Box>
+            <Link
+              p={2}
+              ml="30px"
+              color="white"
+              fontWeight="bold"
+              borderRadius="10px"
+              bg="#F29101"
+              borderColor="#282445"
+              as={NavLink}
+              to="/aboutUs"
+              _hover={{
+                fontWeight: 'semibold',
+                color: '#282445',
+              }}
+            >
+              About us
+            </Link>
           </Box>
-        ))}
-      </SimpleGrid>
-    </Stack>
+        </HStack>
+        {loading && (
+          <HStack justifyContent="center">
+            <Spinner />
+          </HStack>
+        )}
+        <HStack>
+          <Heading size="md">Favorite Products</Heading>
+        </HStack>
+        <SimpleGrid columns={{ base: 1, md: 3, lg: 4 }} p={2}>
+          {renderProducts.map((product) => (
+            <Box key={product.id} w="90%" h="80%" align="center">
+              <Box w="100%" p={2} m={1}>
+                <Image
+                  src={product.img}
+                  alt={product.prodName}
+                  borderRadius="lg"
+                  maxW="70%"
+                  maxH="40%"
+                />
+                <Stack mt="6">
+                  <Heading size="md">{product.prodName}</Heading>
+                  {/* <Text>{product.description}</Text> */}
+                  <Text color="blue.600" fontSize="1xl">
+                    {`$ ${product.price}`}
+                  </Text>
+                </Stack>
+                <ButtonGroup m={1}>
+                  <Button
+                    as={NavLink}
+                    size={{ base: 'xs', md: 'sm' }}
+                    variant="solid"
+                    colorScheme="blue"
+                    to={`/products/${product.id}`}
+                  >
+                    See detail
+                  </Button>
+                  <Button
+                    size={{ base: 'xs', md: 'sm' }}
+                    variant="solid"
+                    bg="tomato"
+                    onClick={() => addProductToCart(product, 1)}
+                  >
+                    Add to cart
+                  </Button>
+                </ButtonGroup>
+              </Box>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Stack>
+    </>
   )
 }
