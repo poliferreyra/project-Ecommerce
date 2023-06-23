cfa1a88a02ef94fb1f9e01b3a2749328797a3825
poliferreyra <paoferreyra.fl@gmail.com>
2023-06-23 13:20:59 -0300

    Add empty cart message

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -7,6 +7,7 @@ import {
   Link,
   Stack,
   StackDivider,
+  Text,
   VStack,
 } from '@chakra-ui/react'
 import { CartDetail } from './CartDetail'
@@ -15,44 +16,103 @@ import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const Cart = () => {
-  const { emptyCart, cartTotalPrice } = useContext(CartContext)
+  const { emptyCart, cartTotalPrice, cart } = useContext(CartContext)
 
   return (
-    <Flex flexDirection="column" minH="75vh" m={6}>
-      <Heading>Shopping Cart</Heading>
-      <Stack flex="1">
-        <CartDetail />
-      </Stack>
-      <VStack
-        divider={<StackDivider borderColor="gray.200" />}
-        spacing={4}
-        align="stretch"
-      >
-        <Box h="40px">
-          <Heading fontSize="2xl">Total : $ {cartTotalPrice()}</Heading>
-        </Box>
-        <Box h="40px">
-          <ButtonGroup spacing="6">
-            <Button variant="outline" color="#DF166D" onClick={emptyCart}>
-              Empty cart
-            </Button>
+    <>
+      {!cart.length ? (
+        <VStack justifyContent="center">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <Text fontWeight="bold">There're not products in the cart</Text>
 
-            <Button bg="#F5E90C" fontWeight="bold">
-              <Link
-                p={2}
-                as={NavLink}
-                to="/checkout"
-                _hover={{
-                  fontWeight: 'semibold',
-                  color: '#DF166D',
-                }}
-              >
-                Checkout
-              </Link>
-            </Button>
-          </ButtonGroup>
-        </Box>
-      </VStack>
-    </Flex>
+          <Button bg="#F5E90C" fontWeight="bold">
+            <Link
+              p={2}
+              as={NavLink}
+              to="/products"
+              _hover={{
+                fontWeight: 'semibold',
+                color: '#DF166D',
+              }}
+            >
+              Shop Now
+            </Link>
+          </Button>
+        </VStack>
+      ) : (
+        <Flex flexDirection="column" minH="75vh" m={6}>
+          <Heading>Shopping Cart</Heading>
+          <Stack flex="1">
+            <CartDetail />
+          </Stack>
+          <VStack
+            divider={<StackDivider borderColor="gray.200" />}
+            spacing={4}
+            align="stretch"
+          >
+            <Box h="40px">
+              <Heading fontSize="2xl">Total : $ {cartTotalPrice()}</Heading>
+            </Box>
+            <Box h="40px">
+              <ButtonGroup spacing="6">
+                <Button variant="outline" color="#DF166D" onClick={emptyCart}>
+                  Empty cart
+                </Button>
+
+                <Button bg="#F5E90C" fontWeight="bold">
+                  <Link
+                    p={2}
+                    as={NavLink}
+                    to="/checkout"
+                    _hover={{
+                      fontWeight: 'semibold',
+                      color: '#DF166D',
+                    }}
+                  >
+                    Checkout
+                  </Link>
+                </Button>
+              </ButtonGroup>
+            </Box>
+          </VStack>
+        </Flex>
+      )}
+      {/* <Flex flexDirection="column" minH="75vh" m={6}>
+        <Heading>Shopping Cart</Heading>
+        <Stack flex="1">
+          <CartDetail />
+        </Stack>
+        <VStack
+          divider={<StackDivider borderColor="gray.200" />}
+          spacing={4}
+          align="stretch"
+        >
+          <Box h="40px">
+            <Heading fontSize="2xl">Total : $ {cartTotalPrice()}</Heading>
+          </Box>
+          <Box h="40px">
+            <ButtonGroup spacing="6">
+              <Button variant="outline" color="#DF166D" onClick={emptyCart}>
+                Empty cart
+              </Button>
+
+              <Button bg="#F5E90C" fontWeight="bold">
+                <Link
+                  p={2}
+                  as={NavLink}
+                  to="/checkout"
+                  _hover={{
+                    fontWeight: 'semibold',
+                    color: '#DF166D',
+                  }}
+                >
+                  Checkout
+                </Link>
+              </Button>
+            </ButtonGroup>
+          </Box>
+        </VStack>
+      </Flex> */}
+    </>
   )
 }
